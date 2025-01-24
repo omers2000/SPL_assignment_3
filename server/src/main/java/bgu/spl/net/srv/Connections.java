f28f264e2d132eb0cf9a8a2eb8c2cb903fdbdbc5
omers2000 <omer.shapira1@gmail.com>
2025-01-24 10:21:21 +0000

    added getters and other method for connectionsimpl

--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -1,6 +1,8 @@
 package bgu.spl.net.srv;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public interface Connections<T> {
 
@@ -8,13 +10,17 @@ public interface Connections<T> {
 
     void send(String channel, T msg);
 
-    void disconnect(int connectionId);
+    void disconnect(int connectionId, String username);
 
     void addConnection(int connectionId, ConnectionHandler<T> handler);
 
-    void addChannel(String channel, int connectionId);
+    void addUser(String username, User<T> user);
 
-    void addUser(User<T> user);
+    User<T> getUser(String username);
 
-    User getUser(String username);
+    void addChannel(String channel);
+
+    ConcurrentHashMap<String, HashMap<Integer, Integer>> getChannels();
+    
+    void cleanClient(int connectionId, String username);
 }
