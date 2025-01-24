514b85f5f56917b232fc0c72c2dce7f9ec049955
omers2000 <omer.shapira1@gmail.com>
2025-01-24 10:23:04 +0000

    implemented methods and added cleanClient method for handling error frame

--- server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -1,28 +1,41 @@
 package bgu.spl.net.impl.stomp;
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.User;
 import bgu.spl.net.srv.ConnectionHandler;
+import bgu.spl.net.srv.Frame;
 
-public class ConnectionsImpl<Frame> implements Connections<Frame> {
+public class ConnectionsImpl<T> implements Connections<Frame> {
 
     private ConcurrentHashMap<Integer, ConnectionHandler<Frame>> clients;
-    private ConcurrentHashMap<String, List<Integer>> channels;
-    private ConcurrentHashMap<String, User> users;
+    private ConcurrentHashMap<String, HashMap<Integer, Integer>> channels;
+    private ConcurrentHashMap<String, User<Frame>> users;
+    private AtomicInteger messageCounter;
 
     public ConnectionsImpl() {
         clients = new ConcurrentHashMap<>();
         channels = new ConcurrentHashMap<>();
         users = new ConcurrentHashMap<>();
+        messageCounter = new AtomicInteger(0);
     }
 
     @Override
     public boolean send(int connectionId, Frame msg) {
+        System.out.println("Command: " + msg.getCommand());
+        for (String key : msg.getHeaders().keySet()) {
+            System.out.println(key + ": " + msg.getKeyByHeader(key));
+        }
+        System.out.println("body:" + msg.getBody());
+        System.out.println("");
+
         ConnectionHandler<Frame> handler = clients.get(connectionId);
         if (handler != null) {
             handler.send(msg);
+            System.out.println("message sent");
             return true;
         }
         return false;
@@ -31,17 +44,20 @@ public class ConnectionsImpl<Frame> implements Connections<Frame> {
     @Override
     public void send(String channel, Frame msg) {
         if (channels.containsKey(channel)) {
-            List<Integer> subscribers = channels.get(channel);
-            for (int connectionId : subscribers) {
-                // Todo: change message to be with the connectionId
+            HashMap<Integer, Integer> subscribers = channels.get(channel);
+            for (int connectionId : subscribers.keySet()) {
+                msg.addHeader("message-id", String.valueOf(this.messageCounter.getAndIncrement()));
                 send(connectionId, msg);
             }
         }
     }
 
     @Override
-    public void disconnect(int connectionId) {
-        clients.remove(connectionId);
+    public void disconnect(int connectionId, String username) {
+        users.get(username).logout();
+        for (HashMap<Integer, Integer> subs : channels.values()){
+            subs.remove(connectionId);
+        }
     }
 
     @Override
@@ -49,21 +65,38 @@ public class ConnectionsImpl<Frame> implements Connections<Frame> {
         clients.put(connectionId, handler);
     }
 
-    @Override
-    public void addChannel(String channel, int connectionId) {
-        channels.putIfAbsent(channel, new CopyOnWriteArrayList<>());
-        channels.get(channel).add(connectionId);
+    public void addChannel(String channel) {
+        channels.putIfAbsent(channel, new HashMap<>());
     }
 
-    @SuppressWarnings("rawtypes")
-    @Override
-    public void addUser(User user) {
-        users.put(user.getUsername(), user);
+    public ConcurrentHashMap<String, HashMap<Integer, Integer>> getChannels(){
+        return channels;
     }
 
-    @SuppressWarnings("rawtypes")
     @Override
-    public User getUser(String username) {
+    public void addUser(String username, User<Frame> user) {
+        users.put(username, user);
+    }
+
+    @Override
+    public User<Frame> getUser(String username) {
         return users.get(username);
+    }
+
+    @Override
+    public void cleanClient(int connectionId, String username) {
+        users.remove(username);
+        for (HashMap<Integer, Integer> subs : channels.values()){
+            subs.remove(connectionId);
+        }
+        ConnectionHandler<Frame> handler = clients.remove(connectionId);
+        handler.addTask(()->{
+            try{
+                handler.close();
+            }
+            catch(IOException x){
+                x.printStackTrace();
+            }
+        });
     }
 }
